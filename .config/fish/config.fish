2f35b8fa41da59ff168242d614b89c18e0eb848a
Muja Mo <muja@foss.land>
2022-03-17 20:17:32 +0100

    alias dotfiles emacs && gcc compile fish shell

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,4 +2,14 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# remove welcome message
 set fish_greeting
+
+
+###### ALIASIS
+# git bare dotfiles
+alias config='/usr/local/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+# open emacs in terminal mode
+alias emacs='emacs -nw'
+# gcc compiler
+alias ggc='gcc -Wall -Wextra -Werror'
